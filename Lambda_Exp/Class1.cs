0ad266b7bd769468946195d5196e6b775314d97e
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-10-04 16:31:04 +0530

    04/10

---
+++ Lambda_Exp/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Exp
+{
+    public delegate int delegate1 (int x);
+   public class Class1
+    {
+
+    }
+}
---
+++ Lambda_Exp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lambda_Exp;
+Console.WriteLine("Hello, World!");
+
+delegate1 d1 = delegate (int x) { return x * x; };                          // Anonymous method 
+                                                                            // (int x)  this is the input parameter data type and  not the return type.
+
+
+int abc = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(d1(abc) );
+
+int z = 2;
+Func<int, int> square = n => n * z;                                         // Lambda expression, in which we dont have to specify the input parameter data type.
+
+Console.WriteLine(square(5));
+
+static void add(delegate1 d11)
+{
+    Console.WriteLine(d11(10));
+}
+
+add((a) => { return a +a; });
+
+
+List<int> list = new List<int>();
+
+static bool random(int a)
+{
+    return true;
+}
+var zz = list.FindAll((a) => { return true; });
